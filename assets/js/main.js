e99ff93929c0ed46b746d4a1e67e8d5417f92ab8
Chen Xianmin <xianmin12@gmail.com>
2023-02-12 09:20:32 +0800

    refactor: back-to-top use pure css

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,6 @@
 // import highlight from './highlight';
 
 /**
- * back to top
- */
-var backToTop = function () {
-  const $backToTop = $('#back-to-top');
-
-  $(window)
-    .scroll(function () {
-      if ($(window)
-        .scrollTop() > 100) {
-        $backToTop.fadeIn(1000);
-      } else {
-        $backToTop.fadeOut(1000);
-      }
-    })
-
-  $backToTop.click(function () {
-    $('body,html')
-      .animate({
-        scrollTop: 0
-      });
-  })
-}
-
-
-/**
  * mobile Navbar
  */
 var mobileNavbar = function () {
@@ -233,7 +208,6 @@ function mapLang(name) {
 
 /* main */
 $(document).ready(function () {
-  backToTop();
   mobileNavbar();
   toc();
   headerAnchor();
