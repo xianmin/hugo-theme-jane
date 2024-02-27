e61518997850d171b1a9c322ce59a30f4633e1e0
Chen Xianmin <xianmin12@gmail.com>
2024-02-27 21:25:22 +0800

    fix: remove Footnote tooltip feature #379

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,10 @@
 import initMobileNavbar from './initMobileNavbar';
 import initToc from './initToc';
 import initHeaderAnchor from './initHeaderAnchor';
-import initFootnoteTooltip from './initFootnoteTooltip';
 
 /* main */
 $(document).ready(function () {
   initMobileNavbar();
   initToc();
   initHeaderAnchor();
-  initFootnoteTooltip();
 });
